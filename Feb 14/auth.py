ebb7a5fdcd0915baa045d847144c5b3bef03cac7
Skyler Austen <me@skylerausten.com>
2023-02-14 17:51:22 -0600

    2/14 Class Materials 2

---
+++ Feb 14/auth.py
@@ -0,0 +1,7 @@
+usernames = ["Skyler", "Kaitlyn"]
+passwords = ["password", "password1"]
+
+# Use a while loop to perform user authentication.
+# Assume each username is unique.
+
+# Hint: .index()
---
+++ Feb 14/mode.py
@@ -0,0 +1,17 @@
+counter_list = [0] *  11
+print(counter_list)
+print("[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]")
+
+# This assumes all values will be between one and ten!
+
+mode_list = [2, 9, 4, 3, 2, 7, 4, 8, 3, 1, 2]
+
+# Task 1: Calculate the number of occurences.
+
+# Task 2: Print the number of occurences for each number, e.g.
+# 1 - 1 Occurrence(s)
+# 2 - 3 Occurrence(s)
+# 3 - 2 Occurrence(s)
+# etc...
+
+# Task 3: Determine the mode.
